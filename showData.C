7d49302347203848611de89aa50a6fc3023e2f3c
himyss <ivanmuzalevskij@gmail.com>
2018-02-13 11:11:25 +0300

    small changes

--- showData.C
+++ showData.C
@@ -7,17 +7,22 @@ void showData(){
 
 	TFile *f1 = new TFile("/home/muzalevsky/expertroot/macro/muzalevsky/kin.root","RECREATE");
 	TTree *tree = new TTree("tree","kin");
-	TLorentzVector n1; //= new TLorentzVector();
-	TLorentzVector n2; //= new TLorentzVector();
-	TLorentzVector h5; //= new TLorentzVector();
-	TLorentzVector h3; //= new TLorentzVector();
+	TLorentzVector *n1 = new TLorentzVector();
+	TLorentzVector *n2 = new TLorentzVector();
+	TLorentzVector *h5 = new TLorentzVector();
+	TLorentzVector *h3 = new TLorentzVector();
+	TLorentzVector *he3 = new TLorentzVector();
+	TLorentzVector *he6 = new TLorentzVector();
 
 	tree->Branch("n1.", &n1); 
 	tree->Branch("n2.", &n2); 
 	tree->Branch("h5.", &h5); 
 	tree->Branch("h3.", &h3);
+	tree->Branch("he3.", &he3);
+	tree->Branch("he6.", &he6);
 
-	Int_t pdg,mID,tID,h3ID;
+
+	Int_t pdg,mID,tID,h3ID,h5ID;
 	Int_t nN,flag,nh3,nEvents;
 	nEvents = 0;
 	for(Int_t i=0; i<t->GetEntries();i++){
@@ -35,9 +40,11 @@ void showData(){
 	    	if(pdg == 1000010050) {
 	    		flag = 1; // if there is an H5 in steck this event is interesting
 	    		nEvents ++ ;
-	    		track->Get4Momentum(h5);
+	    		track->Get4Momentum(*h5);
+	    		h5ID = mID;
 	    		// cout << "found ";
-	    	}	
+	    	}
+
 	    	// if( pdg == 1000010030 ) {h3ID = mID; nh3++;} // if i want to choose the right neutrons i need to know their motherID
 	    } 
 	    if(flag!=1) continue;
@@ -49,7 +56,7 @@ void showData(){
 	    	if(pdg == 1000010030) { // if i want to choose the right neutrons i need to know their motherID
 	    		h3ID = mID; 
 	    		nh3++;
-	    		track->Get4Momentum(h3);
+	    		track->Get4Momentum(*h3);
 	    	} 
 	    } 
 	    if(nh3!=1) continue;  // i want to read events with 1 h3 only
@@ -59,25 +66,28 @@ void showData(){
 	    	pdg = track->GetPdgCode();
 	    	mID = track->GetMotherId();
 
-	    	if( (pdg == 2112) && (mID == h3ID) ) {
+	    	if((pdg == 2112) && (mID == h3ID)) {
 	    		nN++;
-	    		if(nN == 1) track->Get4Momentum(n1);
-	    		if(nN == 2) track->Get4Momentum(n2);
+	    		if(nN == 1) track->Get4Momentum(*n1);
+	    		if(nN == 2) track->Get4Momentum(*n2);
 	    	}
-
+	    	if((pdg == 1000020030) && (mID == h5ID)) {
+	    		track->Get4Momentum(*he3);
+	    	}
+	    	if(pdg == 1000020060) {
+	    		track->Get4Momentum(*he6);
+	    	}
 	    	// cout << track->GetPdgCode() << endl;
 		}
 		if(nN!=2) {cout << nN << " wtf "<< i << endl;}
 		tree->Fill();
 	} // t->GetEntries()
 
-	cout << nEvents;
+	// cout << nEvents;
 	tree->Write();
 	f1->Close();
 	return;
 }
-
-
 	/*	t->GetEntry(1619);
 		for (Int_t iTrack = 0; iTrack < tracks->GetEntriesFast(); iTrack++){
 	    	ERMCTrack* track = (ERMCTrack*)tracks->At(iTrack);
