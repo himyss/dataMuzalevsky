c20d2ff0fc50d76451c54c8f40b4a1c4cf931778
Muzalevsky Ivan <muzalevsky@exolina3.jinr.ru>
2018-05-17 16:03:23 +0300

    filling macro for reading on exolina3

--- exp201803/calib/Si_1/checkCalibration.C
+++ exp201803/calib/Si_1/checkCalibration.C
@@ -1,17 +1,30 @@
-{
-  TFile *f = new TFile("/home/muzalevsky/AculUti/exp1804/calib/si_1mm/SQY_L_full_calibrated_spectra.root");
+void checkCalibration() {
+  Int_t par1,par2;
+  par1=0; par2 = 1;  
+
+  if(par1) {
+      TFile *f = new TFile("/home/muzalevsky/work/exp1803/data/dataCal/SQY_L_full_calibrated_spectra.root");
   const Int_t nhists = 16;
-  TH1F *h[nhists];
-
-
-
+  TH1F *h[nhists],*hsumm;
+  hsumm = new TH1F("hsumm","summ cal spec",4095,0,10);
   TString hname,cName;
   TTree *t = (TTree*)f->Get("AnalysisxTree");
   for(Int_t i=0;i<nhists;i++) {
     hname.Form("HistSQY_L[%d]Efull",i);
-    h[i] = (TH1F*)f->Get(hname.Data()); 
+    h[i] = (TH1F*)f->Get(hname.Data());
+    //cout  << h[i]->GetXaxis()->GetXmax()<< " " << i<< endl;
   }
-  
+  TCanvas *c1 = new TCanvas("c1"," summ cal spec",1000,1000);
+  Float_t content;
+  for(Int_t i=0;i<4095;i++) {
+    content = 0.;
+    for(Int_t j=0;j<nhists;j++) {
+      if(j==0) continue;
+      content += h[j]->GetBinContent(i);    
+    }
+    hsumm->SetBinContent(i,content);
+  }
+
   TF1* g1 = new TF1("g1", "gaus", 4.4, 4.6);
   g1->SetParLimits(0,1.,150.);
   g1->SetParLimits(1,4.,5.);
@@ -31,26 +44,23 @@
   g4->SetParLimits(0,1.,150.);
   g4->SetParLimits(1,7.25,7.65);
   g4->SetParLimits(2,0.1,3.);
-  
-  const Int_t ncan = 4;
-  TCanvas *c[ncan];
-  for(Int_t i=0;i<ncan;i++){
+
+  c1->cd();
+  hsumm->Draw();
+  hsumm->Fit("g1","R+");
+  hsumm->Fit("g2","R+");
+  hsumm->Fit("g3","R+");
+  hsumm->Fit("g4","R+");
+
+  TCanvas *c[4];
+  for(Int_t i=0;i<4;i++){
     cName.Form("c%d",i+1);
-    TCanvas *c[i] = new TCanvas(cName.Data(),"calibrated spectra",1000,1000);
+    c[i] = new TCanvas(cName.Data(),"calibrated spectra",1000,1000);
     c[i]->Divide(2,2);
   }
 
-  /*c1->cd(1);
-  h[0]->Draw();
-  h[0]->Rebin(8);
-  h[0]->GetXaxis()->SetRangeUser(4.,8.);
-  h[0]->Fit("g1","R+");
-  h[0]->Fit("g2","R+");
-  h[0]->Fit("g3","R+");
-  h[0]->Fit("g4","R+");*/
-
 	ofstream outcalfile;
-	outcalfile.open("/home/muzalevsky/AculUti/exp1804/calib/si_1mm/check_cali.txt");
+	outcalfile.open("/home/muzalevsky/work/exp1803/data/dataCal/check_cali.txt");
 	if (!outcalfile.is_open()) {
 		cout <<"Output file was not opened" << endl;
 		return;
@@ -64,7 +74,7 @@
     c[count]->cd(nPad);
 
     h[i]->Draw();
-    h[i]->Rebin(5);
+    h[i]->Rebin(8);
     h[i]->Fit("g1","R+");
     h[i]->Fit("g2","R+");
     h[i]->Fit("g3","R+");
@@ -73,6 +83,121 @@
     c[count]->Update();
     outcalfile << g1->GetParameter(1) << "\t" << g2->GetParameter(1) << "\t" << g3->GetParameter(1) << "\t" << g4->GetParameter(1) << endl; 
   }
+
+  } // if(par1)
+///////////////////////////////////
+  if(par2) {
+	  TTree* 		t;
+	  UShort_t    NeEvent_SQ20[16];
+	  UShort_t    NeEvent_SQX_L[32];
+	  UShort_t    NeEvent_SQY_L[16];
+	  TBranch    *b_NeEvent_SQ20;
+	  TBranch    *b_NeEvent_SQX_L;
+	  TBranch    *b_NeEvent_SQY_L;
+
+	  Float_t parSQX1[32], parSQX2[32],parSQY1[16],parSQY2[16];
+    // Reading cal parameters 
+  //------------------------------------------------------------------------------ 
+   // for 1 mm Si detector
+    TString line1;
+    ifstream myfile1;
+    Int_t count=-2;
+    myfile1.open("/home/muzalevsky/work/exp1803/data/dataCal/SQX_L.cal");
+    while (! myfile1.eof() ){
+		  line1.ReadLine(myfile1);
+      if(count < 0){
+        count++;
+        continue;
+      }
+		  if(line1.IsNull()) break;
+      sscanf(line1.Data(),"%g %g", parSQX1+count,parSQX2+count);
+      count++;
+    }
+    cout << " pars for SQX_L " << endl;
+    for(Int_t i=0;i<32;i++){
+      cout << parSQX1[i] << " " << parSQX2[i] << endl;   
+    }
+   // for 20 mkm Si detector
+    ifstream myfile2;
+    TString line2;
+    count=-2;
+    myfile2.open("/home/muzalevsky/work/exp1803/data/dataCal/SQY_L.cal");
+    while (! myfile2.eof() ){
+		  line2.ReadLine(myfile2);
+      if(count < 0){
+        count++;
+        continue;
+      }
+		  if(line2.IsNull()) break;
+      sscanf(line2.Data(),"%g %g", parSQY1+count,parSQY2+count);
+      count++;
+    }
+    cout << " pars for SQY_L mkm " << endl;
+    for(Int_t i=0;i<16;i++){
+      cout << parSQY1[i] << " " << parSQY2[i] << endl;   
+    }
+return;
+    /*// Creating outfile,outtree
+  //------------------------------------------------------------------------------ 
+	  TFile *f[100];  
+	  TString input_file;
+	  Float_t SQX_L[32],SQ20[16];
+    Int_t n =10,maxE;
+    Long64_t nentries;
+
+	  TFile *fw = new TFile("data/out.root", "RECREATE");
+	  TTree *tw = new TTree("tree", "merged data in energies");
+	  tw->Branch("SQX_L",&SQX_L,"SQX_L[32]/F");
+	  tw->Branch("SQ20",&SQ20,"SQ20[16]/F");
+
+    // Reading input filesm, filling out tree
+  //----------------------------------------------------------------------------------
+    for(Int_t n=10;n<20;n++){
+      input_file.Form("/media/users_NAS/Muzalevsky/exp1804/rootfiles/h5_14_00%d.root",n);		
+	    f[n] = new TFile(input_file.Data());
+	    if (f[n]->IsZombie()) {
+		    cerr << "Input file was not opened " << input_file.Data() << endl;
+		    return 1;
+	    }
+
+	    f[n]->GetObject("AnalysisxTree",t);
+	    t->SetMakeClass(1);
+	    t->SetBranchAddress("NeEvent.SQX_L[32]", NeEvent_SQX_L, &b_NeEvent_SQX_L);
+      t->SetBranchAddress("NeEvent.SQ20[16]", NeEvent_SQ20, &b_NeEvent_SQ20);
+	    nentries = t->GetEntries();
+
+      maxE = nentries;
+      cout<<">>> filling TREE up to "<<maxE<< " event"<<endl;
+	    for (Long64_t jentry=0; jentry<maxE;jentry++) {
+		    t->GetEntry(jentry);
+		    for(Int_t i=0; i<32; i++){
+
+          SQX_L[i]=-1.;// обнуление
+          if(i<16) {
+            SQ20[i]=-1.;
+          }
+
+          if(NeEvent_SQX_L[i]>20){ //cut the pedestal, calculating
+            SQX_L[i] = NeEvent_SQX_L[i]*parSQ2[i] + parSQ1[i];
+          }
+     
+          if(i<14){
+            if(NeEvent_SQ20[i]>150.) { //cut the pedestal, calculating
+              SQ20[i] = NeEvent_SQ20[i]*par202[i] + par201[i];
+            }
+          }
+
+	      } // i<32
+		    tw->Fill();			
+	    }//entries
+    }// nfiles
+
+  // writing outfile
+  //----------------------------------------------------------------------------------
+    fw->cd();
+	  tw->Write();
+	  fw->Close();*/
+  } //if par2
 
   return;
 }
