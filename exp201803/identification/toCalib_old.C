df799835c20a81b63cffa8198389dc3a3366ffed
Muzalevsky Ivan <muzalevsky@exolina3.jinr.ru>
2018-07-20 15:49:46 +0300

    CsI and SQR selection added

--- exp201803/identification/toCalib_old.C
+++ exp201803/identification/toCalib_old.C
@@ -58,12 +58,13 @@ void toCalib_old() {
   cout << endl << " pars for CsR strips" << endl;
   for(Int_t i=0;i<16;i++) cout << parCsR1[i] << " " << parCsR2[i] << endl; 
 
+  Float_t tAngle = 12.;
   //------------------------------------------------------------------------------ 
   TChain *t = new TChain("AnalysisxTree");
 
-  Float_t CsI_L[16],tCsI_L[16],CsI_R[16],tCsI_R[16],SQX_L[32],SQY_L[16],tSQX_L[32],tSQY_L[16],SQX_R[32],SQY_R[16],tSQX_R[32],tSQY_R[16],SQ20[16],tSQ20[16],F3[4],tF3[4],F5[4],tF5[4],
-          nx1,nx2,ny1,ny2,x1[32],x2[32],y1[32],y2[32],ToF,deBeam;
+  Float_t CsI_L[16],tCsI_L[16],CsI_R[16],tCsI_R[16],SQX_L[32],SQY_L[16],tSQX_L[32],tSQY_L[16],SQX_R[32],SQY_R[16],tSQX_R[32],tSQY_R[16],SQ20[16],tSQ20[16],x1,x2,y1,y2,ToF,deBeam,zt,yt,xt;
 
+  Int_t nx1,nx2,ny1,ny2;
 
   UShort_t   NeEvent_CsI_L[16],NeEvent_tCsI_L[16],NeEvent_CsI_R[16],NeEvent_tCsI_R[16],NeEvent_SQX_L[32],NeEvent_SQY_L[16],NeEvent_tSQX_L[32],NeEvent_tSQY_L[16],
              NeEvent_SQX_R[32],NeEvent_SQY_R[16],NeEvent_tSQX_R[32],NeEvent_tSQY_R[16],NeEvent_SQ20[16],NeEvent_tSQ20[16],
@@ -78,7 +79,7 @@ void toCalib_old() {
   Long64_t nentries1;
   Int_t maxE,multY_L,multX_L,multY_R,multX_R,mult20,multY_Lt,multX_Lt,multY_Rt,multX_Rt,mult20t,multCsi_R,multCsi_L,timeF5,thresh_X,thresh_Y,thresh_CsI,nCh_L,nCh_R;
 
-  Int_t ToFflag; //! flags
+  Int_t ToFflag,Csi_Rflag,MWPCflag,SQRflag; //! flags
 
   Float_t maxCsI_R,maxCsI_L;
 
@@ -87,32 +88,32 @@ void toCalib_old() {
   TFile *fw = new TFile("/home/muzalevsky/work/exp1803/data/exp1804/h5_14_id1_test.root", "RECREATE");
   TTree *tw = new TTree("tree", "data");
 
-  /*tw->Branch("CsI_L",&CsI_L,"CsI_L[16]/F");
+  tw->Branch("CsI_L",&CsI_L,"CsI_L[16]/F");
   tw->Branch("tCsI_L",&tCsI_L,"tCsI_L[16]/F");
   tw->Branch("CsI_R",&CsI_R,"CsI_R[16]/F");
   tw->Branch("tCsI_R",&tCsI_R,"tCsI_R[16]/F");
-  tw->Branch("SQX_L",&SQX_L,"SQX_L[32]/F");
+  /*tw->Branch("SQX_L",&SQX_L,"SQX_L[32]/F");
   tw->Branch("tSQX_L",&tSQX_L,"tSQX_L[32]/F");
   tw->Branch("SQY_L",&SQY_L,"SQY_L[16]/F");
   tw->Branch("tSQY_L",&tSQY_L,"tSQY_L[16]/F");
+  tw->Branch("SQ20",&SQ20,"SQ20[16]/F");
+  tw->Branch("tSQ20",&tSQ20,"tSQ20[16]/F");*/
+
   tw->Branch("SQX_R",&SQX_R,"SQX_R[32]/F");
   tw->Branch("tSQX_R",&tSQX_R,"tSQX_R[32]/F");
   tw->Branch("SQY_R",&SQY_R,"SQY_R[16]/F");
   tw->Branch("tSQY_R",&tSQY_R,"tSQY_R[16]/F");
-  tw->Branch("SQ20",&SQ20,"SQ20[16]/F");
-  tw->Branch("tSQ20",&tSQ20,"tSQ20[16]/F");*/
 
   tw->Branch("ToF",&ToF,"ToF/F");
   tw->Branch("deBeam",&deBeam,"deBeam/F");
 
- /* tw->Branch("nx1",&nx1,"nx1/F");
-  tw->Branch("ny1",&ny1,"ny1/F");
-  tw->Branch("nx2",&nx2,"nx2/F");
-  tw->Branch("ny2",&ny2,"ny2/F");
-  tw->Branch("x1",&x1,"x1[32]/F");
-  tw->Branch("x2",&x2,"x2[32]/F");
-  tw->Branch("y1",&y1,"y1[32]/F");
-  tw->Branch("y2",&y2,"y2[32]/F");*/
+  tw->Branch("x1",&x1,"x1/F");
+  tw->Branch("x2",&x2,"x2/F");
+  tw->Branch("y1",&y1,"y1/F");
+  tw->Branch("y2",&y2,"y2/F");
+  tw->Branch("yt",&yt,"yt/F");
+  tw->Branch("xt",&xt,"xt/F");  
+
   tw->Branch("trigger",&trigger,"trigger/I");
 
   tw->Branch("multY_L",&multY_L,"multY_L/I");
@@ -170,7 +171,7 @@ void toCalib_old() {
 	
   maxE = nentries1;
   cout<<">>> filling TREE up to "<<maxE<< " event"<<endl;
-  for (Long64_t jentry=0; jentry<500000;jentry++) {
+  for (Long64_t jentry=0; jentry<5000000;jentry++) {
 	  t->GetEntry(jentry);
     if(jentry%10000000==0) cout << "######## " << jentry << endl;
     // обнуление
@@ -178,69 +179,54 @@ void toCalib_old() {
 
 
     //----------------------------- CsI module
- /*   for(Int_t i=0;i<16;i++) { // обнуление
+    for(Int_t i=0;i<16;i++) { // обнуление
       CsI_R[i] = 0;
       CsI_L[i] = 0;
       tCsI_R[i] = 0;
       tCsI_L[i] = 0;
     }
     multCsi_R = 0;
-    multCsi_L = 0;    
-    //searching for maxAmp of CsI_R and CsI_L
+    multCsi_L = 0;
+    Csi_Rflag = 0;    
+    //searching for maxAmp of CsI_R and CsI_L, time selection NeEvent_tCsI_R[i]>0
+
     maxCsI_R = 0; nCh_R = -10;
     maxCsI_L = 0; nCh_L = -10;
     for(Int_t i=0;i<16;i++) {
-      if(maxCsI_R<NeEvent_CsI_R[i]) {
+      if(maxCsI_R<NeEvent_CsI_R[i] && NeEvent_tCsI_R[i]>0) {
         maxCsI_R = NeEvent_CsI_R[i];
         nCh_R = i;  
       }
-      if(maxCsI_L<NeEvent_CsI_L[i]) {
+      if(maxCsI_L<NeEvent_CsI_L[i] && NeEvent_tCsI_L[i]>0) {
         maxCsI_L = NeEvent_CsI_L[i];
         nCh_L = i;  
       }
     }
+ 
     // calculation of the multiplicity
     for(Int_t i=0;i<16;i++){
       if(NeEvent_CsI_R[i]==maxCsI_R && maxCsI_R>0) multCsi_R++;
       if(NeEvent_CsI_L[i]==maxCsI_L && maxCsI_L>0) multCsi_L++;
     }
-    if(multCsi_R>1){
-      cout << " #### " << multCsi_R << " ####" << endl; 
-      for(Int_t i=0;i<16;i++){
-        cout << NeEvent_CsI_R[i] << " " <<  NeEvent_tCsI_R[i] << endl;
-      }
-    }
     
-
-
-
+    // Csi_R energy threshold
+    if(maxCsI_R<200) Csi_Rflag--;
+    //multiplicity selection
+    if(multCsi_R!=1) Csi_Rflag--;   
+  
     //Fill
-    //if(multCsi_R==1 && multCsi_L==1){
-    if(multCsi_R==1){
-      CsI_R[nCh_R] = NeEvent_CsI_R[nCh_R];
-      tCsI_R[nCh_R] = NeEvent_tCsI_R[nCh_R]*0.3;
-      CsI_L[nCh_L] = NeEvent_CsI_L[nCh_L];
-      tCsI_L[nCh_L] = NeEvent_tCsI_L[nCh_L]*0.3;   
-    }
-*/
-   /* for(Int_t i=0;i<16;i++){
-      CsI_L[i] = NeEvent_CsI_L[i];
-      tCsI_L[i] = NeEvent_tCsI_L[i]*0.3;
-      CsI_R[i] = NeEvent_CsI_R[i];
-      tCsI_R[i] = NeEvent_tCsI_R[i]*0.3;
-    }*/
+    CsI_R[nCh_R] = NeEvent_CsI_R[nCh_R];
+    tCsI_R[nCh_R] = NeEvent_tCsI_R[nCh_R]*0.3;
+    CsI_L[nCh_L] = NeEvent_CsI_L[nCh_L];
+    tCsI_L[nCh_L] = NeEvent_tCsI_L[nCh_L]*0.3;   
 
-   // if(tCsI_L[5]>0) cout << CsI_L[5] << endl;
+    //Csi_Rflag MUST BE = 0 !!!
     //----------------------------- endl of CsI module
 
-  //----------------------------- ToF module
+    //----------------------------- ToF module
     // обнуление
-    for(Int_t i=0;i<4;i++) { 
-      F3[i] = 0.;
-      F5[i] = 0.;
-      tF5[i] = 0.;
-      tF3[i] = 0.;
-    }
+    ToF=0;
+    deBeam=0;
     ToFflag = 0;
 
     for(Int_t i=0;i<4;i++) {
@@ -256,8 +242,77 @@ void toCalib_old() {
     ToF = (NeEvent_tF5[0]+NeEvent_tF5[1]+NeEvent_tF5[2]+NeEvent_tF5[3] - NeEvent_tF3[0]-NeEvent_tF3[1]-NeEvent_tF3[2]-NeEvent_tF3[3])*0.25*0.125 + 89.165;
     if(ToF<100 || ToF>200) ToFflag--; 
     //TOFFLAG MUST BE = 0!!
-    //if(ToFflag!=0) cout << ToFflag << " tofflag " << endl; 
     //----------------------------- end of ToF module
+
+    //----------------------------- MWPC module
+    // обнуление 
+    x1 = -100.;
+		y1 = -100.;
+		x2 = -100.;
+		y2 = -100.;
+		xt = -100.;
+		yt = -100.;
+    MWPCflag = 0;
+
+    if(NeEvent_nx1!=1 || NeEvent_ny1!=1 || NeEvent_nx2!=1 || NeEvent_ny2!=1) MWPCflag--; 
+
+	  x1 = NeEvent_x1[0]*1.25-20.;
+	  y1 = NeEvent_y1[0]*1.25-20.;
+
+	  x2 = NeEvent_x2[0]*1.25-20.;
+	  y2 = NeEvent_y2[0]*1.25-20.;
+
+    xt = (546*x1 + 816*(x2-x1))/(546 - (x2-x1)*tan(TMath::DegToRad()*tAngle));
+	  yt = (y2-y1)*(xt-x1)/(x2-x1) + y1;
+    zt = 546.*(xt-x1)/(x2-x1) - 816;
+
+    //MWPCflag must be = 0!!
+    //----------------------------- end of MWPC module
+
+    
+    //----------------------------- SQ_R module
+
+    // обнуление 
+    for(Int_t i = 0; i<32;i++) {
+      SQX_R[i]=0.;
+      tSQX_R[i]=0.;
+      if(i<16) {
+        SQY_R[i]=0.;
+        tSQY_R[i]=0.;
+      }
+    }
+    SQRflag = 0;
+    multY_R = 0;
+    multX_R = 0;
+
+    //fill
+    for(Int_t i=0; i<32; i++) {
+      SQX_R[i] = NeEvent_SQX_R[i]*parXR2[i] + parXR1[i];
+      if(SQX_R[i]>1.5) multX_R++; 
+      tSQX_R[i] = NeEvent_tSQX_R[i]*0.3;
+      if(i<16){
+        SQY_R[i] = NeEvent_SQY_R[i]*parYR2[i] + parYR1[i];
+        if(SQY_R[i]>1.5) multY_R++; 
+        tSQY_R[i] = NeEvent_tSQY_R[i]*0.3;
+      }
+    }
+    
+    // energy threshold 1.5 MeV
+    if(multY_R<1 || multX_R<1) SQRflag--;
+    // time gates
+    for(Int_t i=0;i<32;i++){ 
+      if(i>16){ // CFD LED times (walk)
+        if(tSQX_R[i]*0.3-tF5[0]*0.125 < -60 || tSQX_R[i]*0.3-tF5[0]*0.125>60) SQRflag--;
+        if(tSQY_R[i]*0.3-tF5[0]*0.125 < -100 || tSQY_R[i]*0.3-tF5[0]*0.125>100) SQRflag--;
+      }
+      if(i>15){ // both times by CFD 
+        if(tSQX_R[i]*0.3-tF5[0]*0.125 < 30 || tSQX_R[i]*0.3-tF5[0]*0.125>110) SQRflag--;
+      }
+    }
+
+    // SQRflag must be = 0 !!
+    //----------------------------- end of SQ_R module
+
 
 
 
@@ -310,7 +365,7 @@ void toCalib_old() {
 */
     trigger = NeEvent_trigger;
 	  //if(multX_R>0 && multY_R>0 && timeF5>0 && multCsi_R>0 && thresh_Y==1 && thresh_X==1 && thresh_CsI==1) {
-      if(ToFflag==0) tw->Fill();
+      if(ToFflag==0 && Csi_Rflag==0 && MWPCflag==0) tw->Fill();
    // }			
   }//entries
   fw->cd();
