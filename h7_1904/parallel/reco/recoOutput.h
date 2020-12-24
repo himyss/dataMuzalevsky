a7fb4151d6956cf1e1bdae2bd7d4f8f0b2da691f
himyss <ivanmuzalevsky@gmail.com>
2020-12-24 15:31:52 +0300

    new reco

---
+++ h7_1904/parallel/reco/recoOutput.h
@@ -0,0 +1,23 @@
+Float_t targetTime,diafTime;
+
+// out TLorentzVector's
+TLorentzVector lv_he8,lv_h7,lv_he3,lv_h3;
+TLorentzVector lv_h3_CMH7,lv_h7_CMR;
+
+TLorentzVector lv_he4,lv_h6;
+TLorentzVector lv_h3_CMH6,lv_h6_CMR;
+
+TLorentzVector lv_he7,lv_he7_CMR,lv_h3_CMHe7;
+
+//------------------------------------------------------------------------------------------------
+// Neutron
+Float_t aND,tacND,tND,tND_cal,ND_time_cal[32],ND_energy[32],eNeutron;
+Int_t neutron;
+
+//------------------------------------------------------------------------------------------------
+// h7
+Float_t angle_he3_h7,angle_h3_h7,angle_he3_he8,angle_h7_he8;
+
+//h6
+Float_t angle_he4_h6,angle_he4_he8,angle_h3_he8;
+Float_t angle_h3_h6;
